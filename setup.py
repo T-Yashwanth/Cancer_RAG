f9f0384f9bd89deff5c57f85790483d6e45b5390
T-Yashwanth <yashwanthtelukuntla@gmail.com>
2025-02-07 12:22:05 -0600

    base working model

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cancer_rag",
+    version="0.1.0",
+    description="A Retrieval-Augmented Generation (RAG) system for cancer-related document analysis.",
+    author="T-Yashwanth",
+    packages=find_packages(),
+    python_requires=">=3.8",
+
+)
--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
 import os
 
 base_directory  = os.path.abspath(os.path.join(os.path.dirname(__file__),'..'))
+
 path =  os.path.join(base_directory, "data", "national-cancer-plan-508.pdf")
-#path = r"C:\Users\yash\Downloads\Projects\Health care RAG\data\national-cancer-plan-508.pdf"
 
 embedding_model_name="all-MiniLM-L6-v2"
 
