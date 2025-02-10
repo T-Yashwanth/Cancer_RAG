858c89bef75a950466c5937c04bc3e230777a988
T-Yashwanth <yashwanthtelukuntla@gmail.com>
2025-02-09 20:20:58 -0600

    updated setupfile

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="cancer_rag",
-    version="0.1.0",
-    description="A Retrieval-Augmented Generation (RAG) system for cancer-related document analysis.",
+    version="0.2.0",
+    description="A Retrieval-Augmented Generation (RAG) system for cancer-related document analysis. Dosent have a UI but borks in CLI and able to track the conversation and history",
     author="T-Yashwanth",
     packages=find_packages(),
     python_requires=">=3.8",
